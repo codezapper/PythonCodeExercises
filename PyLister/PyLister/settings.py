df209250e50209add3c68acd0f362716e734c5bd
codezapper <gabriele.nazeri@tiscali.it>
2016-11-05 00:19:14 +0100

    First commit with DB model

--- PyLister/PyLister/settings.py
+++ PyLister/PyLister/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'lister.apps.ListerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- PyLister/lister/models.py
+++ PyLister/lister/models.py
@@ -2,4 +2,14 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Song(models.Model):
+    album = models.CharField(max_length=256)
+    artist = models.CharField(max_length=256)
+    image_file = models.CharField(max_length=256)
+    year = models.DateTimeField(default=1900)
+    rating = models.IntegerField(default=0)
+    title = models.CharField(max_length=256)
+
+    def __str__(self):
+        return '{} ({}) by {}'.format(self.title, self.year, self.artist)
