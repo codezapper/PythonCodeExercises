c8832b81b7f753b1812ac7f6363d3bb5649166a6
codezapper <gabriele.nazeri@tiscali.it>
2016-11-09 23:13:49 +0100

    Added indexer

---
+++ PyLister/file_indexer.py
@@ -0,0 +1,25 @@
+import glob2
+import taglib
+import sqlite3
+
+
+def get_files():
+    return glob2.glob('Music/**/*.mp3')
+
+
+def update_db():
+    db_main = sqlite3.connect('db.sqlite3')
+    cursor = db_main.cursor()
+    songs = []
+    for file in get_files():
+        song = taglib.File(file)
+        songs.append((song.tags['ALBUM'][0], song.tags['ARTIST'][
+            0], '', song.tags['DATE'][0], 0, song.tags['TITLE'][0], file))
+    cursor.execute('DELETE FROM lister_song')
+    cursor.executemany(
+        'INSERT INTO lister_song (album, artist, image_file, year, rating, title, path) VALUES(?, ?, ?, ?, ?, ?, ?)', songs)
+
+    db_main.commit()
+
+if __name__ == '__main__':
+    update_db()
