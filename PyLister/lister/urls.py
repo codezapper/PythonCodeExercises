73b91570f7e63cdc39f9888ddde69e38648a4b6a
codezapper <gabriele.nazeri@tiscali.it>
2016-11-14 22:40:48 +0100

    !WIP! Adding albums layout

--- PyLister/lister/urls.py
+++ PyLister/lister/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^songs/$', views.index, name='songs'),
+    url(r'^albums/$', views.albums, name='albums'),
+    url(r'^artists/$', views.artists, name='artists'),
+    url(r'^years/$', views.years, name='years'),
     # ex: /polls/5/details/
     url(r'^detail/([0-9]+)/$', views.detail, name='detail'),
     # ex: /polls/5/vote/
--- PyLister/lister/views.py
+++ PyLister/lister/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Count
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader
@@ -11,6 +12,30 @@ def index(request):
     template = loader.get_template('lister/index_with_menu.html')
     context = {'songs_list': songs_list, }
     return HttpResponse(template.render(context, request))
+
+
+def songs(request):
+    songs_list = [song
+                  for song in Song.objects.order_by('-year')]
+    template = loader.get_template('lister/index_with_menu.html')
+    context = {'songs_list': songs_list, }
+    return HttpResponse(template.render(context, request))
+
+
+def albums(request):
+    albums_list = [album
+                   for album in Song.objects.values('album').annotate(dcount=Count('album'))]
+    template = loader.get_template('lister/index_with_menu.html')
+    context = {'songs_list': albums_list, }
+    return HttpResponse(template.render(context, request))
+
+
+def artists(request):
+    return HttpResponse('ARTISTS')
+
+
+def years(request):
+    return HttpResponse('YEARS')
 
 
 def detail(request, song_id):
