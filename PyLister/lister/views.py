d7466105cd7552c89278dc4edb7f4a2cb5a87c6e
codezapper <gabriele.nazeri@tiscali.it>
2016-11-16 23:31:17 +0100

    Created additional layouts

--- PyLister/lister/views.py
+++ PyLister/lister/views.py
@@ -13,7 +13,7 @@ def index(request):
 
 def songs(request):
     songs_list = [song
-                  for song in Song.objects.order_by('-artist', 'album', 'track_number')]
+                  for song in Song.objects.order_by('artist', 'album', 'track_number')]
     template = loader.get_template('lister/index_with_menu.html')
     context = {'songs_list': songs_list, }
     return HttpResponse(template.render(context, request))
@@ -36,11 +36,35 @@ def albums(request):
 
 
 def artists(request):
-    return HttpResponse('ARTISTS')
+    cursor = connection.cursor()
+    cursor.execute(
+        '''SELECT artist, count(artist) FROM lister_song GROUP BY artist ORDER BY artist''')
+    row = cursor.fetchone()
+
+    artists_list = []
+    while (row):
+        artists_list.append(
+            {'artist': row[0], 'count': row[1]})
+        row = cursor.fetchone()
+    template = loader.get_template('lister/artists_with_menu.html')
+    context = {'artists_list': artists_list, }
+    return HttpResponse(template.render(context, request))
 
 
 def years(request):
-    return HttpResponse('YEARS')
+    cursor = connection.cursor()
+    cursor.execute(
+        '''SELECT year, count(year) FROM lister_song GROUP BY year ORDER BY year''')
+    row = cursor.fetchone()
+
+    years_list = []
+    while (row):
+        years_list.append(
+            {'year': row[0], 'count': row[1]})
+        row = cursor.fetchone()
+    template = loader.get_template('lister/years_with_menu.html')
+    context = {'years_list': years_list, }
+    return HttpResponse(template.render(context, request))
 
 
 def detail(request, song_id):
